ebad4e58b6f2be66c8392481c2f503c5ef148768
Thiago Carvalho De Medeiros <tmedeiros@cusa.canon.com>
2024-08-13 12:26:00 -0300

    Gráfico de linha do tempo passa a ter 3 linhas, gráficos de barra formatados, corrigido bug da seleção de data

--- Streamlit-Authenticator/tests/unificado.py
+++ Streamlit-Authenticator/tests/unificado.py
@@ -196,15 +196,21 @@ if st.session_state["authentication_status"]:
             if 'selected_date_range' not in st.session_state:
                 st.session_state.selected_date_range = None
             
-            modo_grafico = st.sidebar.multiselect(label="Selecione Gráficos Desejados", options=["Gráficos do gerente", "Graficos do cliente"], default="Gráficos do gerente")
+            modo_grafico = st.sidebar.selectbox(label="Selecione Gráficos Desejados", options=["Gráficos do gerente", "Gráficos do cliente"], index = 0)
             st.write(modo_grafico)
+            if modo_grafico == "Gráficos do cliente":
+                index_cliente = 1
+            else:
+                index_cliente = 0
+
+
 
             # Primeiro filtro: Cliente
             enterprise_options = ["Todos"] + list(data['EnterpriseName'].unique())
             st.session_state.selected_enterprise = st.sidebar.selectbox(
                 "Selecione o nome da empresa",
                 options=enterprise_options,
-                index=enterprise_options.index(st.session_state.selected_enterprise)
+                index= index_cliente
             )
             
             # Filtrar os modelos com base no cliente selecionado
@@ -253,9 +259,15 @@ if st.session_state["authentication_status"]:
             )
 
             # Filtrar os dados com base no período de datas selecionado
+            # Verificar se a tupla tem dois elementos antes de desempacotá-la
+            if len(st.session_state.selected_date_range) != 2:
+                st.warning("Por favor, selecione um intervalo de datas completo.")
+                st.stop()
+            else:
+               pass 
+
             start_date, end_date = st.session_state.selected_date_range
             df_selection = filtered_data[(filtered_data['data'] >= start_date) & (filtered_data['data'] <= end_date)]
-
             #st.dataframe(df_selection) exibe todas colunas do dataframe filtrado
 
             #Início do bloco de gráficos do administrador
@@ -263,7 +275,7 @@ if st.session_state["authentication_status"]:
             gerente_container = st.container()
             cliente_container = st.container()
 
-            if modo_grafico == ["Gráficos do gerente"]:
+            if modo_grafico == "Gráficos do gerente":
                 with gerente_container:
 
                     df_selection['Número de Série - Cliente'] = df_selection['SerialNumber'] + " - " + df_selection['EnterpriseName']
@@ -300,7 +312,110 @@ if st.session_state["authentication_status"]:
                         "total": "Total Impresso"
                     })
                     st.plotly_chart(bar3)
-            
+            elif modo_grafico == "Gráficos do cliente":
+                with cliente_container:
+                    #Início do bloco de gráficos
+                    A4Pb = df_selection['pb_peq'].sum()
+                    A4Cor = df_selection['cor_peq'].sum()
+                    A3Pb = df_selection['pb_grande'].sum()
+                    A3Cor = df_selection['cor_grande'].sum()
+                    total = df_selection['total'].sum()
+                    equipamentos = df_selection['SerialNumber']
+                    cores = ['#1f77b4', '#ff7f0e']
+                    st.title("Total por formato/cor")
+                    col1, col2 = st.columns(2)
+
+                    
+                    st.title("Total por equipamento")
+                    df_grouped_total = df_selection.groupby('SerialNumber', as_index=False).sum(numeric_only=True)
+                    bar1 = px.bar(df_grouped_total, y='total', x='SerialNumber', title='Total por Equipamento', color='SerialNumber')
+                    st.plotly_chart(bar1)
+                    
+                    
+                    with col1:
+                        pz1 = px.pie(names=('P&B', 'COR'), values=[A4Pb, A4Cor], title='Impressões A4', color_discrete_sequence=cores)
+                        st.plotly_chart(pz1)
+
+                    if A3Pb != 0 or A3Cor != 0:
+                        with col2:
+                            pz2 = px.pie(names=('P&B', 'COR'), values=[A3Pb, A3Cor], title='Impressões A3', color_discrete_sequence=cores)
+                            st.plotly_chart(pz2)
+
+                    # Criar novas colunas para a produção colorida e PB
+                    filtered_data['Producao_Cor'] = filtered_data['cor_peq'] + filtered_data['cor_grande']
+                    filtered_data['Producao_PB'] = filtered_data['pb_peq'] + filtered_data['pb_grande']
+
+                    # Agrupar por data e somar as produções
+                    timeline_data_cor = filtered_data.groupby('data')['Producao_Cor'].sum().reset_index()
+                    timeline_data_pb = filtered_data.groupby('data')['Producao_PB'].sum().reset_index()
+                    timeline_data_total = filtered_data.groupby('data')['total'].sum().reset_index()
+
+                    # Fazer o merge dos DataFrames resultantes
+                    timeline_data = pd.merge(timeline_data_cor, timeline_data_pb, on='data', suffixes=('_Cor', '_PB'))
+                    timeline_data = pd.merge(timeline_data, timeline_data_total, on='data')
+
+                    if timeline_data['Producao_Cor'].sum() == 0:
+                        timeline_data.drop(columns=['Producao_Cor'], inplace=True)
+
+                    if timeline_data['Producao_PB'].sum() == 0:
+                        timeline_data.drop(columns=['Producao_PB'], inplace=True)
+
+                    if timeline_data['total'].sum() == 0:
+                        timeline_data.drop(columns=['total'], inplace=True)
+
+                    # Gráfico de linha com 3 linhas, uma para 'Producao_Cor', outra para 'Producao_PB' e outra para 'Producao total' 
+                    st.title("Produção por cor ao Longo do Tempo")
+                    fig = px.line(timeline_data, x='data', y=timeline_data.columns[1:], 
+                                labels={'value': 'Produção', 'data': 'Data'}, 
+                                title='Produção ao Longo do Tempo')
+
+                    # Exibindo o gráfico
+                    st.plotly_chart(fig)
+
+                    
+                    resumo = df_selection.groupby(df_selection['SerialNumber']).agg({
+                    'pb_peq': 'sum',
+                    'pb_grande': 'sum',
+                    'cor_peq': 'sum',
+                    'cor_grande': 'sum',
+                    'total': 'sum'
+                    }).reset_index()
+
+                    
+                    # Renomeia as colunas do dataframe
+                    resumo = resumo.rename(columns={
+                        'SerialNumber': 'Equipamento',
+                        'pb_peq': 'P&B A4',
+                        'pb_grande': 'P&B A3',
+                        'cor_peq': 'COR A4',
+                        'cor_grande': 'COR A3',
+                        'total': 'TOTAL'
+                    })
+
+                    cols_to_format = ['P&B A4', 'P&B A3', 'COR A4', 'COR A3', 'TOTAL']
+                    resumo[cols_to_format] = resumo[cols_to_format].applymap(lambda x: f"{x:,}".replace(",", "."))
+
+                    st.title("Resumo da Produção por Equipamento")
+                    resumo = resumo.reset_index(drop=True)
+                    st.dataframe(resumo, hide_index= True)
+
+
+                    resumo_total ={ 'P&B A4':df_selection['pb_peq'].sum(), 
+                                'P&B A3':df_selection['pb_grande'].sum(), 
+                                'COR A4':df_selection['cor_peq'].sum(), 
+                                'COR A3':df_selection['cor_grande'].sum(), 
+                                'TOTAL':df_selection['total'].sum()}
+                    
+                    df_resumo_total = pd.DataFrame(resumo_total, index=[0])
+
+                    # Substituindo vírgulas por pontos na formatação dos números
+                    df_resumo_total = df_resumo_total.applymap(lambda x: f"{x:,}".replace(",", "."))
+                    
+                    st.title("Resumo da Produção Total") 
+                    df_resumo_total = df_resumo_total.reset_index(drop=True)
+                    st.dataframe(df_resumo_total, hide_index= True)
+
+
         else:  #Bloco de dashboard do cliente
             st.write('Você possui acesso somente as suas informações')
 
@@ -401,6 +516,11 @@ if st.session_state["authentication_status"]:
             st.sidebar.write("Período selecionado:", formatted_start_date, "-", formatted_end_date)
 
             # Filtrar os dados com base no período de datas selecionado
+            if len(st.session_state.selected_date_range) != 2:
+                st.warning("Por favor, selecione um intervalo de datas completo.")
+                st.stop()
+            else:
+               pass 
             start_date, end_date = st.session_state.selected_date_range
             df_selection = filtered_data[(filtered_data['data'] >= start_date) & (filtered_data['data'] <= end_date)]
 
@@ -418,7 +538,8 @@ if st.session_state["authentication_status"]:
 
             
             st.title("Total por equipamento")
-            bar1 = px.bar(df_selection, y='total', x='SerialNumber', title='Total por Equipamento', color='SerialNumber')
+            df_grouped_total = df_selection.groupby('SerialNumber', as_index=False).sum(numeric_only=True)
+            bar1 = px.bar(df_grouped_total, y='total', x='SerialNumber', title='Total por Equipamento', color='SerialNumber')
             st.plotly_chart(bar1)
             
             
@@ -434,37 +555,34 @@ if st.session_state["authentication_status"]:
             # Criar novas colunas para a produção colorida e PB
             filtered_data['Producao_Cor'] = filtered_data['cor_peq'] + filtered_data['cor_grande']
             filtered_data['Producao_PB'] = filtered_data['pb_peq'] + filtered_data['pb_grande']
-            
 
+            # Agrupar por data e somar as produções
             timeline_data_cor = filtered_data.groupby('data')['Producao_Cor'].sum().reset_index()
             timeline_data_pb = filtered_data.groupby('data')['Producao_PB'].sum().reset_index()
-            
+            timeline_data_total = filtered_data.groupby('data')['total'].sum().reset_index()
 
-
+            # Fazer o merge dos DataFrames resultantes
             timeline_data = pd.merge(timeline_data_cor, timeline_data_pb, on='data', suffixes=('_Cor', '_PB'))
+            timeline_data = pd.merge(timeline_data, timeline_data_total, on='data')
 
+            if timeline_data['Producao_Cor'].sum() == 0:
+                timeline_data.drop(columns=['Producao_Cor'], inplace=True)
 
-            # Agora, vamos criar o gráfico de linha com duas linhas, uma para 'Producao_Cor' e outra para 'Producao_PB'
-            st.title("Produção P&B X Colorido ao Longo do Tempo")
-            fig = px.line(timeline_data, x='data', y=['Producao_Cor', 'Producao_PB'], 
+            if timeline_data['Producao_PB'].sum() == 0:
+                timeline_data.drop(columns=['Producao_PB'], inplace=True)
+
+            if timeline_data['total'].sum() == 0:
+                timeline_data.drop(columns=['total'], inplace=True)
+
+            # Gráfico de linha com 3 linhas, uma para 'Producao_Cor', outra para 'Producao_PB' e outra para 'Producao total' 
+            st.title("Produção por cor ao Longo do Tempo")
+            fig = px.line(timeline_data, x='data', y=timeline_data.columns[1:], 
                         labels={'value': 'Produção', 'data': 'Data'}, 
                         title='Produção ao Longo do Tempo')
 
             # Exibindo o gráfico
             st.plotly_chart(fig)
-
-            #Gráfico de linha do tempo por total
-            st.title("Produção Total ao Longo do Tempo")
-            timeline_data_total = filtered_data.groupby('data')['total'].sum().reset_index()
-            line_chart_total = px.line(
-                timeline_data_total,
-                x='data',
-                y='total',
-                title='Produção Total ao Longo do Tempo'
-            )
-            st.plotly_chart(line_chart_total)
-
-            
+          
             resumo = df_selection.groupby(df_selection['SerialNumber']).agg({
             'pb_peq': 'sum',
             'pb_grande': 'sum',
@@ -507,28 +625,28 @@ if st.session_state["authentication_status"]:
             df_resumo_total = df_resumo_total.reset_index(drop=True)
             st.dataframe(df_resumo_total, hide_index= True)
 
-            # Rodapé com HTML e CSS
-            footer = """
-            <style>
-            .footer {
-                position: fixed;
-                left: 0;
-                bottom: 0;
-                width: 100%;
-                background-color: #f1f1f1;
-                text-align: center;
-                padding: 3px;
-                font-size: 14px;
-                color: #555;
-                height: 30px;  /* Ajusta a altura do rodapé */
-            }
-            </style>
-            <div class="footer">
-                <p>© 2024 Canon do Brasil. Todos os direitos reservados.</p>
-            </div>
-            """
+        # Rodapé com HTML e CSS
+        footer = """
+        <style>
+        .footer {
+            position: fixed;
+            left: 0;
+            bottom: 0;
+            width: 100%;
+            background-color: #f1f1f1;
+            text-align: center;
+            padding: 3px;
+            font-size: 14px;
+            color: #555;
+            height: 30px;  /* Ajusta a altura do rodapé */
+        }
+        </style>
+        <div class="footer">
+            <p>© 2024 Canon do Brasil. Todos os direitos reservados.</p>
+        </div>
+        """
 
-            st.markdown(footer, unsafe_allow_html=True)
+        st.markdown(footer, unsafe_allow_html=True)
 
             #st.dataframe(df_selection)
             
